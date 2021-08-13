7b8ce4b0e6e729e28529abd4eddaa6df7b0b7716
Hemant Gauhai <77909457+itsmehemantgauhai01@users.noreply.github.com>
2021-08-13 11:06:31 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+let saveEl=document.getElementById("save-el")
+let countEl=document.getElementById("count-el")
+let count=0
+
+function increment(){
+    count+=1
+    countEl.innerHTML=count
+}
+
+function save(){
+    let countStr=count+" - "
+    saveEl.innerHTML+=countStr
+    countEl.innerHTML=0
+    count=0
+}
